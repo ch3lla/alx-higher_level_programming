cd22c5e69aa91a784dee7bdbbbddc3220ffbc2e3
ch3lla <chukwumaemmanuela03@gmail.com>
2023-03-14 05:19:27 -0700

    seventh task

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 6-print_matrix_integer.py
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for col in row:
+            print("{:d}".format(col), end=" " if col != row[-1] else "")
+        print()
