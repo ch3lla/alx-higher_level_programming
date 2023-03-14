86293b859a4dbb1beaa1411f6b1c95e8bcd91995
ch3lla <chukwumaemmanuela03@gmail.com>
2023-03-14 06:37:36 -0700

    tenth task

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 9-max_integer.py
+def my_integer(my_list=[]):
+    """Return the biggest integer of a list."""
+    if len(my_list) == 0:
+        return (None)
+    big = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > big:
+            big = my_list[i]
+    return (big)
