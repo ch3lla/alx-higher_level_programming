2d90254b4e1db463d689a0c63de7974437db817f
ch3lla <chukwumaemmanuela03@gmail.com>
2023-03-27 17:04:58 -0700

    done with the tasks under this project

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+     """Prints an integer with "{:d}".format()."""
+     try:
+          print("{:d}".format(value))
+          return (True)
+     except (TypeError, ValueError):
+          print("Exception: {}".format(sys.exec_info()[1]), file=sys.stderr)
+          return (False)
